4bf07156a05d5358c1df09b37217f068e85bfd0e
Lito <lito@eordes.com>
2021-02-26 09:03:20 +0100

    Added actingAs to Command Abstract

--- app/Domains/Shared/Command/CommandAbstract.php
+++ app/Domains/Shared/Command/CommandAbstract.php
@@ -3,7 +3,9 @@
 namespace App\Domains\Shared\Command;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Domains\Shared\Traits\Factory;
 use App\Exceptions\ValidatorException;
 
@@ -59,7 +61,7 @@ abstract class CommandAbstract extends Command
     final protected function checkOptions(array $keys): void
     {
         foreach ($keys as $key) {
-            if (strlen((string)$this->option($key)) === 0) {
+            if ($this->option($key) === null) {
                 throw new ValidatorException(sprintf('Option "%s" is required', $key));
             }
         }
@@ -71,5 +73,25 @@ abstract class CommandAbstract extends Command
     final protected function requestWithOptions(): Request
     {
         return request()->replace($this->arguments() + $this->options());
+    }
+
+    /**
+     * @param null|int|\Illuminate\Contracts\Auth\Authenticatable $user = null
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    final protected function actingAs($user = null): Authenticatable
+    {
+        $model = config('auth.providers.users.model');
+
+        if (is_null($user)) {
+            $user = new $model;
+        } elseif (is_int($user)) {
+            $user = $model::findOrFail($user);
+        }
+
+        Auth::login($user);
+
+        return $this->auth = $user;
     }
 }
