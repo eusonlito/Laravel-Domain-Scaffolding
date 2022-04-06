e9df6a09835d8b5cac72fb1de22a8c0db1606a6d
Lito <lito@eordes.com>
2022-04-06 11:42:34 +0200

    Updated Ignition settings

--- config/ignition.php
+++ config/ignition.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'theme' => env('IGNITION_THEME', 'auto'),
+    'theme' => env('IGNITION_THEME', 'light'),
 
     /*
     |--------------------------------------------------------------------------
